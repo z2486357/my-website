b252c9fcc7cb919293cb279131726199e251a6c0
z2486357 <z2486357@gmail.com>
2019-08-13 22:58:54 -0400

    building my website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-website';
+export class AppComponent implements OnInit {
+  
+  constructor(private titleService: Title){}
+
+  ngOnInit(){
+    this.titleService.setTitle('Yu-Cheng Chang');
+  }
+
 }
